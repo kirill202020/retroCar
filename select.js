9c4bfe94c29403028897fa1bce26e41845b9a891
kirill202020 <91893674+kirill202020@users.noreply.github.com>
2025-03-20 12:25:21 +0100

    Add files via upload

---
+++ select.js
@@ -0,0 +1,14 @@
+const carName = document.querySelector('.choose-name'),
+	carImg = document.querySelector('.choose-img'),
+	carSelect = document.querySelector('.choose-select')
+function change() {
+	let carSelectVal = document.querySelector('.choose-select').value
+	carName
+	switch (key) {
+		case value:
+			break
+
+		default:
+			break
+	}
+}
